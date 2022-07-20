ec46b80c9f5aa924937183530eae3ae6737402c4
Philip Lages <psilvaelages@gmail.com>
2022-07-20 17:04:22 -0300

    Add requirements 6 and 7

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
+import planets from './data/planets';
+import Missions from './components/Missions';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <section>
         <Header />
-        <SolarSystem />
+        <SolarSystem planets={ planets } />
+        <Missions />
       </section>
     );
   }
--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import PlanetCard from './PlanetCard';
 import Title from './Title';
 
 class SolarSystem extends Component {
   render() {
+    const { planets } = this.props;
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        <div className="cards">
+          {planets.map(({ name, image }) => (
+            <PlanetCard
+              planetName={ name }
+              planetImage={ image }
+              key={ name }
+            />))}
+        </div>
       </div>
     );
   }
 }
+
+SolarSystem.propTypes = {
+  planets: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string,
+    image: PropTypes.string,
+  })).isRequired,
+};
 
 export default SolarSystem;
